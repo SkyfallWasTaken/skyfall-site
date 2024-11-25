255dc4b395885aa9e0579ab6f4d3d1181547a817
Mahad Kalam <mahadkalam1234@gmail.com>
2024-11-25 21:51:13 +0000

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://skyfall.dev',
+  integrations: [tailwind(), sitemap()],
   output: 'hybrid',
   adapter: cloudflare()
 });
