871ad4efdc2c7e58603c634e49dfb086539f8942
Mahad Kalam <mahadkalam1234@gmail.com>
2024-11-30 14:40:31 +0000

    Add favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,25 @@ import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import favicons from "astro-favicons";
 import vercel from "@astrojs/vercel/serverless";
+
+import { SITE_TITLE, SITE_DESCRIPTION } from "./src/constants";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://skyfall.dev",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    favicons({
+      masterPicture: "./src/img/rust.svg",
+      emitAssets: true,
+      appName: SITE_TITLE,
+      appDescription: SITE_DESCRIPTION,
+    }),
+  ],
+
   markdown: {
     shikiConfig: {
       themes: {
