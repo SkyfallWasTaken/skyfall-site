e5dee7c992ac7594f745b019e8a160c8fec0d042
Mahad Kalam <mahadkalam1234@gmail.com>
2024-11-30 11:47:30 +0000

    Switch to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@
 import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
-import cloudflare from '@astrojs/cloudflare';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,5 +28,5 @@ export default defineConfig({
   },
 
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: vercel()
 });
