984594b008e82b0edc6fe688088cd0c9c696d5b3
Mahad Kalam <mahadkalam1234@gmail.com>
2024-12-01 23:05:48 +0000

    bit more work on og image

--- src/components/og/image.tsx
+++ src/components/og/image.tsx
@@ -3,10 +3,12 @@ import { formattedDate } from "../../util";
 
 export default function (props: CollectionEntry<"blog">) {
   return (
-    <div tw="flex flex-col w-full h-full p-12 items-center text-center justify-center text-text bg-base">
-      <div tw="flex font-bold text-8xl mb-4">{props.data.title}</div>
-      <div tw="flex text-5xl mb-12">{props.data.description}</div>
-      <div tw="flex text-3xl">
+    <div tw="flex flex-col w-full h-full pt-16 text-white bg-indigo-700">
+      <div tw="flex flex-1 flex-col w-full h-full border-2 px-16 justify-center">
+        <div tw="flex font-bold text-8xl mb-24">{props.data.title}</div>
+        <div tw="flex text-4xl mb-8">{props.data.description}</div>
+      </div>
+      <div tw="flex px-16 items-center text-3xl mt-auto border-[1px] border-t-white py-12">
         {formattedDate(new Date(props.data.pubDate))}
       </div>
     </div>
