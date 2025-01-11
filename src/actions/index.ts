aac3ae0998b7eaab9ca00f6a3c0aba29ae45553f
Mahad Kalam <mahadkalam1@proton.me>
2025-01-11 12:28:07 +0000

    Update formatting

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,21 +3,21 @@ import { DISCORD_USER_ID } from "astro:env/client";
 import { fetchUserData as fetchLanyardUserData } from "lanyard-wrapper";
 
 export const server = {
-    discord: {
-        profile: defineAction({
-            // important to be "json" in this case because it's not being called from a form submit.
-            accept: "json",
-            handler: async (_) => {
-                try {
-                    const data = await fetchLanyardUserData(DISCORD_USER_ID);
-                    return data;
-                } catch {
-                    throw new ActionError({
-                        code: "INTERNAL_SERVER_ERROR",
-                        message: "Error fetching data from Lanyard",
-                    });
-                }
-            },
-        }),
-    },
+  discord: {
+    profile: defineAction({
+      // important to be "json" in this case because it's not being called from a form submit.
+      accept: "json",
+      handler: async (_) => {
+        try {
+          const data = await fetchLanyardUserData(DISCORD_USER_ID);
+          return data;
+        } catch {
+          throw new ActionError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Error fetching data from Lanyard",
+          });
+        }
+      },
+    }),
+  },
 };
