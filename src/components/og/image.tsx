c98fd0184eb683c23640baa835f11abc88b0c99f
Mahad Kalam <mahadkalam1@proton.me>
2025-03-20 22:07:43 +0000

    Fix OG image spacing

--- src/components/og/image.tsx
+++ src/components/og/image.tsx
@@ -5,9 +5,9 @@ export default function (props: CollectionEntry<"blog">) {
   return (
     <div tw="flex flex-col w-full h-full pt-16 text-white bg-indigo-600">
       <div tw="flex flex-1 flex-col w-full h-full border-2 px-16 justify-between">
-        <div tw="flex flex-col justify-center flex-1">
-          <h1 tw="flex font-extrabold text-8xl">{props.data.title}</h1>
-          <p tw="flex text-4xl tracking-tight mt-8">{props.data.description}</p>
+        <div tw="flex flex-col gap-6 justify-center flex-1">
+          <div tw="flex font-extrabold text-8xl">{props.data.title}</div>
+          <div tw="flex text-4xl tracking-tight mt-8">{props.data.description}</div>
         </div>
         <p tw="flex items-center text-3xl py-12">
           {formattedDate(new Date(props.data.pubDate))}
