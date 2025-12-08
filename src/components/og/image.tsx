06473678b77c7b452a50d9f71424c423a63a62aa
Mahad Kalam <mahadkalam1234@gmail.com>
2025-12-08 13:23:36 +0000

    Add slack-mass-leave + format + remove unused deps

--- src/components/og/image.tsx
+++ src/components/og/image.tsx
@@ -7,7 +7,9 @@ export default function (props: CollectionEntry<"blog">) {
       <div tw="flex flex-1 flex-col w-full h-full border-2 px-16 justify-between">
         <div tw="flex flex-col gap-6 justify-center flex-1">
           <div tw="flex font-extrabold text-8xl">{props.data.title}</div>
-          <div tw="flex text-4xl tracking-tight mt-8">{props.data.description}</div>
+          <div tw="flex text-4xl tracking-tight mt-8">
+            {props.data.description}
+          </div>
         </div>
         <p tw="flex items-center text-3xl py-12">
           {formattedDate(new Date(props.data.pubDate))}
---
+++ src/content/projects/slack-mass-leave.md
@@ -0,0 +1,8 @@
+---
+title: Slack Mass Leave
+tagline: A tool for mass leaving Slack channels, featuring last read sorting, multi-token support, and channel search.
+mainImage: "@assets/img/projects/slack-mass-leave.png"
+pinned: false
+tools: ["Typescript", "React"]
+url: "https://github.com/SkyfallWasTaken/slack-mass-leave"
+---
